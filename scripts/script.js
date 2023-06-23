a5b2a9839077e7183bde06dd9fdf1c6bd663da85
ssharmapavitra <80170208+ssharmapavitra@users.noreply.github.com>
2023-06-23 17:27:03 +0530

    Append Responses

--- scripts/script.js
+++ scripts/script.js
@@ -6,9 +6,13 @@ const qlist = document.getElementById("qlist");
 const enter = document.getElementById("enter");
 const resolve = document.getElementById("resolve");
 const selectedQ = document.getElementById("selectedQ");
+const commentBtn = document.getElementById("rSubmit");
+const nameIp = document.getElementById("name");
+const comment = document.getElementById("comment");
 
 //Variables
 var id = 0;
+let cid = 1;
 
 subject.value = "Sample 1";
 qtext.value = "This is a sample Question asked";
@@ -28,6 +32,12 @@ subBtn.addEventListener("click", () => {
 
 //add new question to list
 function addNewQuestion(sub, question) {
+	addQuestion(sub, question);
+	addToStorage(id, sub, question);
+}
+
+//add question to HTML
+function addQuestion(sub, question) {
 	id++;
 	//Create div
 	let newList = document.createElement("div");
@@ -51,13 +61,11 @@ function addNewQuestion(sub, question) {
 	newList.appendChild(subhead);
 	newList.appendChild(newq);
 	qlist.appendChild(newList);
-
-	addToStorage(id, sub, question);
 }
 
-//add to local Storage
+//add subject and question to local Storage
 function addToStorage(id, sub, question) {
-	let obj = { subject: sub, question: question };
+	let obj = { subject: sub, question: question, responses: [] };
 	obj = JSON.stringify(obj);
 	localStorage.setItem(id, obj);
 	localStorage.setItem(0, id);
@@ -68,7 +76,7 @@ function load() {
 	for (let i = 1; i <= size; i++) {
 		let obj = localStorage.getItem(i);
 		obj = JSON.parse(obj);
-		addNewQuestion(obj.subject, obj.question);
+		addQuestion(obj.subject, obj.question);
 	}
 }
 
@@ -77,10 +85,110 @@ function onResolve(qid) {
 	enter.style.display = "none";
 	resolve.style.display = "flex";
 
+	//Question
 	let question = document.getElementById(`qid${qid}`).cloneNode(true);
 	question.style.border = "0px";
 	selectedQ.innerHTML = "";
 	selectedQ.appendChild(question);
+
+	//clearing response
+	let table = document.getElementById("table");
+	table.innerHTML = "";
+
+	//Response
+	addResponse(qid);
+
+	commentBtn.setAttribute("onclick", `onComment(${id})`);
 }
 
-// localStorage.clear();
+//adding response to table
+function addResponse(qid) {
+	let obj = localStorage.getItem(qid);
+	obj = JSON.parse(obj);
+	let resp = document.getElementById("table");
+
+	for (let i = 0; i < obj.responses.length; i++) {
+		//Div for response and name
+		let div = document.createElement("div");
+		div.setAttribute("class", "response");
+
+		//Name
+		let name = document.createElement("h3");
+		name.innerText = obj.responses[i].name;
+		div.appendChild(name);
+
+		//Response
+		let response = document.createElement("p");
+		response.innerText = obj.responses[i].response;
+		div.appendChild(response);
+
+		//response
+		let row = document.createElement("tr");
+		let respon = document.createElement("td");
+		respon.appendChild(div);
+		row.appendChild(respon);
+
+		//upvote
+		let upvote = document.createElement("td");
+		upvote.innerText = obj.responses[i].upvote;
+		row.appendChild(upvote);
+
+		//downvote
+		let downvote = document.createElement("td");
+		downvote.innerText = obj.responses[i].favorite;
+		row.appendChild(downvote);
+
+		//favorite
+		let favorite = document.createElement("td");
+		favorite.innerText = obj.responses[i].favorite;
+		row.appendChild(favorite);
+
+		//appending row
+		resp.appendChild(row);
+	}
+}
+
+//Adding event listner to button
+function onComment(cid) {
+	let obj = localStorage.getItem(cid);
+	obj = JSON.parse(obj);
+	let response = {};
+
+	//getting values from boxes
+	if (nameIp.value == "" || comment.value == "") {
+		alert("Please Enter a value");
+		return;
+	}
+
+	response.name = nameIp.value;
+	response.response = comment.value;
+	response.upvote = 0;
+	response.favorite = false;
+	obj.responses.push(response);
+	obj = JSON.stringify(obj);
+	console.log(obj);
+	localStorage.setItem(id, obj);
+
+	onResolve(cid);
+}
+
+localStorage.clear();
+
+/*
+Schema
+{
+ id: {
+	sub: " ",
+	question : " ".
+	responses :[
+		{ 
+			name: " ",
+			response: " ",
+			upvote : " ",
+			favorite: " ",
+		}.....{}
+	]
+ }
+}
+
+*/
